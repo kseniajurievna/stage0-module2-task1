ef0bceca7076597da169fe167a887a535bcdb562
kseniakaratchenia <62858256+kseniakaratchenia@users.noreply.github.com>
2023-02-07 20:49:36 -0800

    Syntax assignment

---
+++ src/main/java/com/mjc/stage0/BrokenApp.java
@@ -0,0 +1,7 @@
+package com.mjc.stage0;
+
+public class BrokenApp {
+  public static void main(String[] args) {
+    System.out.println("fixMe");
+  }
+}
---
+++ src/main/java/com/mjc/stage0/PrintWordsApp.java
@@ -0,0 +1,9 @@
+package com.mjc.stage0;
+
+import java.sql.SQLOutput;
+
+public class PrintWordsApp {
+  public static void main(String[] args) {
+    System.out.println("java" + "\n" + "is" +"\n"+"smth." );
+  }
+}
